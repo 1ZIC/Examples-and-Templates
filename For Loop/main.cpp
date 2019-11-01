9f6908a518357dea674ed0951eea5775abc78b0f
1ZIC <Isaacrdarrah@gmail.com>
2019-11-01 09:53:43 -0500

    Add files via upload

---
+++ For Loop/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+///     for(int;condition; increment)
+///     {
+///         statment(s);
+///     }
+for(int i=10; i>=1; i--)  //+= means the number will count by for example, fives or whatever number you place in. ++ means it will count by ones.
+{
+cout<<i<<endl;
+
+}
+    return 0;
+}
---
+++ Increment.etc/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=25;
+    x*=5;
+    cout<<x--<<endl;
+    cout<<x<<endl;
+    return 0;
+}
